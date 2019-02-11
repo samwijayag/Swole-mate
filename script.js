45866fb0ee32efa411ef01e3986be98773f00676
samwijayag <sam.wijayag@gmail.com>
2019-02-10 19:01:40 -0800

    made nav sticky

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	// Sticky Nav
+	
+	$('.swole-mate').waypoint(function(direction){
+		if(direction == 'down'){
+			$('nav').addClass('sticky');
+		}else{
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset:'60px'
+	});
+
+	// Scroll
+	$('.btn-full').click(function(){
+		$('html,body').animate({scrollTop: $('.subscribe').offset().top()},1000);
+	})
+
+});
+
+
