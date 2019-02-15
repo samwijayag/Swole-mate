6e2496b4ba087e2174566f56c0b98b3330c4b718
samwijayag <sam.wijayag@gmail.com>
2019-02-14 23:34:37 -0800

    added full functionality to navbar

--- script.js
+++ script.js
@@ -21,9 +21,21 @@ $(document).ready(function(){
 	})
 
 	//nav
-	$('.scroll-swole-mate').click(function(){
-		$('html,body').animate({scrollTop: $('.swole-mate').offset().top},1000);
-	})
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+    });
+
 
 
 
