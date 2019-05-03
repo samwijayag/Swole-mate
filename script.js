77280fa7cf938a155bc37264888a86984de2b085
samwijayag <sam.wijayag@gmail.com>
2019-05-03 16:40:59 -0700

    added new color

--- script.js
+++ script.js
@@ -21,22 +21,20 @@ $(document).ready(function(){
 	})
 
 	//nav
-	$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+	        || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-    });
-
-
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		           if (target.length) {
+		             $('html,body').animate({
+		                 scrollTop: target.offset().top
+		            }, 1000);
+		        	return false;
+		    }
+	    }
+	});
 
 
 
